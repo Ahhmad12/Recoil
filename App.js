561883cb7d60d50228349668bdfbd3da72379fc1
Ahmad Naeem <ahmadnaeem26831@gmail.com>
2023-02-06 23:09:58 +0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import RootStack from "./Components/Screens/RootStack";
+import { NavigationContainer } from "@react-navigation/native";
+import { RecoilRoot } from "recoil";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Myapp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isVisible: true,
+    };
+  }
+  render() {
+    return (
+      <RecoilRoot>
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </RecoilRoot>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-  container: {
+  SplashScreen_RootView: {
+    justifyContent: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+
+  SplashScreen_ChildView: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00BCD4",
+    flex: 1,
   },
 });
